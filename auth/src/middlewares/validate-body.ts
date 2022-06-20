4e6e79cb46c4e49250c9dbe4d43bfe467cef692e
Max-im <pogidaevmo@gmail.com>
2022-06-20 20:35:02 +0300

    review

--- auth/src/middlewares/validate-body.ts
+++ auth/src/middlewares/validate-body.ts
@@ -2,7 +2,6 @@ import {Request, Response, NextFunction} from 'express';
 import { validationResult } from 'express-validator';
 import { ReqValidationError } from '../errors/validation-error';
 
-
 export const validateBody = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
 
