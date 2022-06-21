9cca4bebcc3685e4103ab958f9c473832ed79c8f
Max-im <pogidaevmo@gmail.com>
2022-06-22 00:45:01 +0300

    add signed in message

--- auth/src/middlewares/validate-body.ts
+++ auth/src/middlewares/validate-body.ts
@@ -5,7 +5,6 @@ import { ReqValidationError } from '../errors/validation-error';
 export const validateBody = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     
-    console.log(req.body, errors.array())
     if (!errors.isEmpty()) {
         throw new ReqValidationError(errors.array());
     }
