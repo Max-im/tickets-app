1064acf9397d93fc1cf4f20c4d7780720ffb6fcd
Max-im <pogidaevmo@gmail.com>
2022-06-22 00:44:38 +0300

    add signed in message

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import buildClient from '../api/build-client';
 
-export default function index() {
+const Index = ({ currentUser }) => {
   return (
     <div>
-      <h1>index</h1>
+      <h1>{currentUser ? 'You are signed in' : 'You are NOT signed in'}</h1>
     </div>
   );
-}
+};
+
+Index.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
+};
+
+export default Index;
