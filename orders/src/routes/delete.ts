3cfe58c89da53938b78c7c70c2ce7d1cda3ab14e
Max-im <pogidaevmo@gmail.com>
2022-06-25 13:02:30 +0300

    delete order route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,13 +1,39 @@
-import { requireAuth } from '@mpozhydaiev-tickets/common';
+import {
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  UnauthError,
+  validateBody,
+} from '@mpozhydaiev-tickets/common';
 import { Router, Request, Response } from 'express';
+import { param } from 'express-validator';
+import mongoose from 'mongoose';
 import { Order } from '../models/Order';
 
 const router = Router();
 
-router.delete('/api/orders/:id', requireAuth, async (req: Request, res: Response) => {
-  const order = Order.deleteOne({ _id: req.params.id });
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  [
+    param('orderId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Invalid Order Id'),
+  ],
+  validateBody,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId);
 
-  res.send(order);
-});
+    if (!order) throw new NotFoundError();
+    if (order.userId !== req.currentUser!.id) throw new UnauthError();
+
+    order.status = OrderStatus.Canceled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as ordersDeleteRouter };
